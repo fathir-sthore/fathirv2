68e6b7b4960a1603f2b97d5e39617d04703c3bb6
fathir-sthore <fathirsthore@gmail.com>
2025-07-19 03:58:32 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,25 @@
+/* 
+
+=======================================
+
+SC CREATED BY : ©RELLZ
+YT OWNER : https://youtube.com/@RELLZSILENT
+TELE OWNER : @rellzSilent
+CONTACT : 083135977026 / 085138527460
+
+NOTE : Hargai Pengembang Dengan Cara Tidak Rename/Delete Credit Di Atas, Gunakan Semaumu Tapi Jangan Mengambil Karya Ku. Jika Ingin Merepost Kembali Tolong Tag Pengembang Dimanapun Itu Terima Kasih.
+
+=======================================
+
+*/
+const settings = {
+  owner: '7879293312', // ganti id telegram lu
+  token: '7549229318:AAG7HBAkRMfPTdAE_6Nanx6Yaypq3JWnEiI', // Token Bot Lu
+  domain: 'domain_lu', // domain
+  plta: 'ptla_lu', // plta yang sesuai
+  pltc: 'pltc_lu', // pltc yang sesuai
+  loc: '1', // Isi dengan lokasi yang diinginkan
+  eggs: '15',
+};
+
+module.exports = settings;
